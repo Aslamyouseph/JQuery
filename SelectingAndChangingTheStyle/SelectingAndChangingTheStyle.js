13874b10dfb5835f0fd14dc6683b2e2d579b84f4
Aslam youseph <aslamyouseph@gmail.com>
2025-05-17 11:21:27 +0530

    Selecting And Changing The Style And Text Of The Element

--- SelectingAndChangingTheStyle/SelectingAndChangingTheStyle.js
+++ SelectingAndChangingTheStyle/SelectingAndChangingTheStyle.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
   // Accessing the first heading to change the text
-  $("#button1").click(function () {
+  $("#button1").on("click", function () {
     $("#heading1").text("Hello Aslam Youseph");
   });
-  // Accessing the Second heading to change the Color
-  $(".button2").click(function () {
+
+  // Accessing the second heading to change the color
+  $(".button2").on("click", function () {
     $(".heading2").css("color", "blue");
   });
 });
